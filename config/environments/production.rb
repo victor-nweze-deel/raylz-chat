aab5a42a4e061abdf468ac6744c6460cd279c5ae
ProfJigsaw <dhagodfather@gmail.com>
2019-02-23 14:34:18 +0100

    Add redis for prod fix :rocket:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # websockets server url
+  config.web_socket_server_url = "wss://rails-chat-with-cable.herokuapp.com/cable"
 end
